0ff1a3fdfe46626da7fde48cb2ad95270a2ea352
virtual <2661006892@qq.com>
2025-03-17 15:31:15 +0800

    添加Cpp功能包示例

---
+++ helloworld_cpp/src/helloworld.cpp
@@ -0,0 +1,15 @@
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char **argv)
+{
+  // 初始化 ROS2
+  rclcpp::init(argc, argv);
+  // 创建节点
+  auto node = rclcpp::Node::make_shared("helloworld_node");
+  // 输出文本
+  RCLCPP_INFO(node->get_logger(), "hello world!");
+  rclcpp::spin(node);
+  // 释放资源
+  rclcpp::shutdown();
+  return 0;
+}
